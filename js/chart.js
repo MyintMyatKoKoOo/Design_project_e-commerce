250a41840fb7451af1592a51c686ef86f1f28455
mmkkko <mymyint704@gmail.com>
2020-09-24 23:33:23 +0630

    update

--- js/chart.js
+++ js/chart.js
@@ -35,3 +35,4 @@ let myLineChart = new Chart(line, {
     }
     
 });
+
--- js/chartpage.js
+++ js/chartpage.js
@@ -1,5 +1,5 @@
 let donut= document.getElementById('myChartdon').getContext('2d');
-let myChart = new Chart(donut, {
+let myDonutChart = new Chart(donut, {
     type: 'doughnut',
     data: {
         labels: ["Complaint","Error Report","Succes"],
@@ -33,4 +33,33 @@ let myLineChart = new Chart(line, {
         }]
     }
     
+});
+
+let bar = document.getElementById('myChartbar').getContext('2d');
+let myBarChart = new Chart(bar, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Views',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
 });
